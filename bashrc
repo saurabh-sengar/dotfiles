42b718f5d71666b1b9c8f8c483edab5a95411c9b
Saurabh Sengar <ssengar@linux.microsoft.com>
2022-06-09 16:32:46 +0530

    add nvim support
    Signed-off-by: Saurabh Sengar <ssengar@linux.microsoft.com>

--- bashrc
+++ bashrc
@@ -115,4 +115,4 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-
+alias vim='nvim'
--- extras/ubuntu-dev.sh
+++ extras/ubuntu-dev.sh
@@ -10,7 +10,7 @@ sudo apt-add-repository -y ppa:neovim-ppa/unstable
 
 # install build-essential git gcc
 sudo apt update
-sudo apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev ssh git vim net-tools zstd neovim python3-pip exuberant-ctags clangd ripgrep
+sudo apt install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev ssh git vim net-tools zstd neovim python3-pip exuberant-ctags clangd ripgrep curl
 
 mkdir ~/.config/nvim
 mkdir /work/
@@ -20,3 +20,6 @@ cp ../bashrc ~/.bashrc
 cp ../gitconfig ~/.gitconfig
 cp ../vimrc ~/.vimrc
 cp ../init.vim ~/.config/nvim/init.vim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
